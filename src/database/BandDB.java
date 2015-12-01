3de98cfb91fc6858a1b9afe0427bc31d96405a59
= <palmerjoshua2013@fau.edu>
2015-12-01 18:36:05 -0500

    added function to get bands who need a certain instrument

--- src/database/BandDB.java
+++ src/database/BandDB.java
@@ -26,6 +26,11 @@ public class BandDB extends Database {
         db.addNeededInstruments("73f89842", needed);
     }
 
+    public NodeList getBandsByNeeded(String instrument) throws Exception {
+        String xpathString = "//need/instrument[contains(.,'"+instrument+"')]/../..";
+        return getNodeListByXPath(xpathString);
+    }
+
     public void addBand(Element userElement) throws Exception {
         String email = userElement.getElementsByTagName("email").item(0).getTextContent(),
                 id = userElement.getAttribute("id");
